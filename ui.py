e40c9d24ada896d1272ac45cc251b360a889c6f1
Alex Deidda <alex.deidda@aledesign.co.uk>
2021-02-15 16:59:42 +0000

    Run video function added

--- ui.py
+++ ui.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 14 16:29:29 2021
-
-@author: cecilia
-"""
 import subprocess
 import tkinter as tk
-
 window = tk.Tk()
 window.wm_attributes('-fullscreen','true')
 
 def startfile():
-    firefox = "/usr/lib/libreoffice/program/scalc"
-    subprocess.Popen([firefox])
-
-tk.Button(window, text="Open", command=startfile).pack()
-
+    app = "/usr/bin/navit"
+    subprocess.Popen([app])
+    
+def run():
+    # -fs -> fullscreen
+    # tv: -> source path
+    subprocess.run(["mplayer", "-fs", "tv:///dev/video0"])
+    
+        
+#def starvideo():
+#    rcam = "/usr/mplayer tv:///dev/video0"
+#    subprocess.Popen([rcam])
+        
+#tk.Button(window, text="Open App", command=startfile).pack()
+tk.Button(window, text="video", command=run).pack()
+#tk.Button(window, text="Open Video", command=startvideo).pack()
 tk.Button(window, text="Quit", command=window.destroy).pack()
 
-window.mainloop()
+window.mainloop() 
