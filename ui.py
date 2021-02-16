c5af36dfb4434bd0a9bb3e460bd3f290f9779ebd
Alex Deidda <alex.deidda@aledesign.co.uk>
2021-02-16 17:29:01 +0000

    camera test

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import subprocess
 import tkinter as tk
+import cv2
 
 ## main window ##########################################
 window = tk.Tk()
@@ -28,7 +29,24 @@ def startfile():
 def run():
     # -fs -> fullscreen
     # tv: -> source path
-    subprocess.run(["mplayer", "-fs", "tv:///dev/video0"])
+    #subprocess.run(["mplayer", "-fs", "tv:///dev/video0"])
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
     
 
 ## Buttons ############################################
